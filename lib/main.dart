614a4ecbf2a528b9c883d4d9375c9703c46e225c
Cristiano <cristianovelkan@gmail.com>
2022-11-21 10:01:29 -0300

    linearProgressIndicator widget

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,25 @@ class _TaskState extends State<Task> {
                   ],
                 ),
               ),
-              Text('Nível: $nivel', style: TextStyle(color: Colors.white, fontSize: 16),),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 200,
+                      child: LinearProgressIndicator(
+                        color: Colors.white,
+                        value: nivel/10
+                      )
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Nível: $nivel', style: TextStyle(color: Colors.white, fontSize: 16),),
+                  ),
+                ],
+              ),
             ],
           ),
 
