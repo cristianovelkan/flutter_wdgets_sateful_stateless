c2485ba4aefd0c7a2d44417930bc97e569dcdbe3
Cristiano <cristianovelkan@gmail.com>
2022-11-21 09:27:41 -0300

    stateless widgets

---
+++ android/app/src/main/kotlin/app/desenvolva/test/flutter_wdgets_sateful_stateless/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.desenvolva.test.flutter_wdgets_sateful_stateless
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tarefas'),
+        ),
+        body: ListView(
+          children: [
+            Task('Aprender Flutter: Aula 401, depois de já ter aprendido Dart corretamente'),
+            Task('Andar de Bike'),
+            Task('Correr'),
+            Task('Correr'),
+            Task('Correr'),
+            Task('Correr'),
+            Task('Dormir')
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: (){}),
+      ),
+    );
+  }
+}
+
+//Trecho de código suprimido
+
+class Task extends StatelessWidget {
+  final String nome;
+  const Task(this.nome, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black26,
+                  width: 72,
+                  height: 100,
+                ),
+                Container(
+                  width: 200,
+                    child: Text(
+                      nome,
+                      style: TextStyle(
+                          fontSize: 24,
+                          overflow: TextOverflow.ellipsis
+                      ),
+                    )
+                ),
+                ElevatedButton(
+                    onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
